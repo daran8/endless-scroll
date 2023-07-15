d13273dfdf2ba4a06d5b1ab0c453c40dc7980dc0
Bharanidaran <bharanidaran.balamurugan@ideas2it.com>
2023-07-15 12:12:04 +0530

    task: jenkins sample after changing name

--- infiniteScroll.js
+++ infiniteScroll.js
@@ -1,12 +1,12 @@
 function scroll(data, id) {
     document.addEventListener("scroll", function () {
-        var container = document.getElementById(id);
+        var selectedElement = document.getElementById(id);
 
         data.forEach(function (element) {
             if (window.scrollY + window.innerHeight >=
                 document.documentElement.scrollHeight &&
-                container !== null) {
-                container.appendChild(element);
+                selectedElement !== null) {
+                selectedElement.appendChild(element);
             }
         });
     });
